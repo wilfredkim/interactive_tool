f50637541f9b272359b453c94c2d10c3463893e0
Wilfred <wilfredkim5@gmail.com>
2025-12-21 13:59:16 +0300

    updates on audit table

--- src/main/java/com/interactivetool/wilfred/interactivetool/InteractiveToolApplication.java
+++ src/main/java/com/interactivetool/wilfred/interactivetool/InteractiveToolApplication.java
@@ -2,8 +2,10 @@ package com.interactivetool.wilfred.interactivetool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InteractiveToolApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/interactivetool/wilfred/interactivetool/entity/BaseEntity.java
+++ src/main/java/com/interactivetool/wilfred/interactivetool/entity/BaseEntity.java
@@ -1,17 +1,20 @@
 package com.interactivetool.wilfred.interactivetool.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
